050fd2c39936e480e5cdb71d4ef58702aad3dd11
MedAliKrifa <daliolimona@gmail.com>
2023-09-02 14:33:45 +0100

    task_3/

---
+++ react_intro/task_3/dashboard/src/utils.js
@@ -0,0 +1,17 @@
+export function getFullYear() {
+    const currentYear = new Date().getFullYear();
+    return currentYear;
+}
+
+export function getFooterCopy(isIndex) {
+    if (isIndex === true) {
+        return "Holberton School";
+    } else {
+        return "Holberton School main dashboard";
+    }
+}
+export function getLatestNotification(){
+    return(
+        '<strong>Urgent requirement</strong> - complete by EOD'
+    );
+}
