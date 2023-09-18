53f808aabca66b1910682925137350dff07835a5
MedAliKrifa <daliolimona@gmail.com>
2023-09-18 17:40:04 +0100

    task_0/

--- react_state/task_0/dashboard/src/App/App.js
+++ react_state/task_0/dashboard/src/App/App.js
@@ -1,135 +1,158 @@
-import React from 'react';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications';
-import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
-import BodySection from '../BodySection/BodySection';
-import { getLatestNotification } from '../utils/utils';
-import { StyleSheet, css } from 'aphrodite';
+import React, { Component } from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Footer from "../Footer/Footer";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
+import { StyleSheet, css } from "aphrodite";
 
 const listCourses = [
-  {id: 1, name: "ES6", credit: 60},
-  {id: 2, name: "Webpack", credit: 20},
-  {id: 3, name: "React", credit: 40}
-]
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
 
 const listNotifications = [
-  {id: 1, type: "default", value: "New course available"},
-  {id: 2, type: "urgent", value: "New resume available"},
-  {id: 3, type: "urgent", __html: {__html: getLatestNotification()}},
-]
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+];
 
-class App extends React.Component {
+document.body.style.margin = 0;
+
+class App extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
-      isLoggedIn: props.isLoggedIn || false,
-      displayDrawer: false,
+    super(props);
+    this.handleKeyCombination = this.handleKeyCombination.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.state = { displayDrawer: false };
+  }
+
+  handleKeyCombination(e) {
+    if (e.key === "h" && e.ctrlKey) {
+      alert("Logging you out");
+      this.props.logOut();
     }
-    this.logOut = props.logOut;
-    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this)
-    this.handleHideDrawer = this.handleHideDrawer.bind(this)
   }
 
   handleDisplayDrawer() {
-    this.setState({displayDrawer: true});
+    this.setState({ displayDrawer: true });
   }
 
   handleHideDrawer() {
-    this.setState({displayDrawer: false});
+    this.setState({ displayDrawer: false });
   }
-  
-  handleKeyDown(e) {
-    if (e.ctrlKey && e.code == "KeyH") {
-      e.preventDefault()
-      alert("Logging you out");
-      this.logOut();
-    }
-  }
-  
+
   componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keydown", this.handleKeyCombination);
   }
-  
+
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown);
+    document.removeEventListener("keydown", this.handleKeyCombination);
   }
-  
+
   render() {
+    const { isLoggedIn, logOut } = this.props;
+    const { displayDrawer } = this.state;
+
     return (
-      <React.Fragment>
-      <Notifications listNotifications={listNotifications} displayDrawer={this.state.displayDrawer} handleDisplayDrawer={this.handleDisplayDrawer} handleHideDrawer={this.handleHideDrawer}/>
-      <div className={css(styles.flexFullHeight)}>
-      <Header className={css(styles.appHeader)}/>
-      { this.state.isLoggedIn ? (
-        <BodySectionWithMarginBottom title="Course list">
-        <CourseList listCourses={listCourses} className={css(styles.bodyHeight)}/>
-        </BodySectionWithMarginBottom>
-        ) : (
-          <BodySectionWithMarginBottom title="Log in to continue">
-          <Login />
-          </BodySectionWithMarginBottom>
-          )
-        }
-        <BodySection title="News from the school">
-        <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti 
-        atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
-        </p>
-        </BodySection>
-        <Footer className={css(styles.footer)}/>
+      <>
+        <Notifications
+          listNotifications={listNotifications}
+          displayDrawer={displayDrawer}
+          handleDisplayDrawer={this.handleDisplayDrawer}
+          handleHideDrawer={this.handleHideDrawer}
+        />
+        <div className={css(styles.container)}>
+          <div className={css(styles.app)}>
+            <Header />
+          </div>
+          <div className={css(styles.appBody)}>
+            {!isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom>
+            )}
+          </div>
+          <BodySection title="News from the School">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+          </BodySection>
+
+          <div className={css(styles.footer)}>
+            <Footer />
+          </div>
         </div>
-        </React.Fragment>
-        )
-      }
-    }
-    
-    
-    App.propTypes = {
-      isLoggedIn: PropTypes.bool,
-      logOut: PropTypes.func
-    }
-    
-    App.defaultProps = {
-      isLoggedIn: false,
-      logOut: () => {
-        return
-      }
-    }
-    
-    const styles = StyleSheet.create({
-      flexFullHeight: {
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-      },
-      
-      bodyHeight: {
-        height: "70%",
-      },
-      
-      appHeader: {
-        display: "flex",
-        alignItems: "center",
-        borderBottom: "3px solid #e0354b",
-        color: "#e0354b",
-        fontSize: "1.5rem",
-        fontWeight: "bold",
-        height: "25%",
-      },
-      
-      footer: {
-        position: "absolute",
-        bottom: "0",
-        width: "100%",
-        borderTop: "3px solid var(--holberton-red) !important",
-        textAlign: "center",
-        padding: "16px 0",
-      }
-    });
-    
-    
-    export default App;
+      </>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+const cssVars = {
+  mainColor: "#e01d3f",
+};
+
+const screenSize = {
+  small: "@media screen and (max-width: 900px)",
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "calc(100% - 16px)",
+    marginLeft: "8px",
+    marginRight: "8px",
+  },
+
+  app: {
+    borderBottom: `3px solid ${cssVars.mainColor}`,
+  },
+
+  appBody: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  footer: {
+    borderTop: `3px solid ${cssVars.mainColor}`,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    position: "fixed",
+    bottom: 0,
+    fontStyle: "italic",
+    [screenSize.small]: {
+      position: "static",
+    },
+  },
+});
+
+export default App;
